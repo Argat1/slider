e56c81c49868fbdddd50728d3812f3c14bf6d79d
lukasz jablonski <lukasz.jablonski44@gmail.com>
2024-04-03 09:35:54 +0200

    added images class

---
+++ slider/slider/EditImagePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace slider
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditImagePage : ContentPage
+    {
+        public EditImagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ slider/slider/Images.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace slider
+{
+    public class Images
+    {
+        public ImageSource ImageSource { get; set; }
+        public string FilePath { get; set; }
+    }
+}
